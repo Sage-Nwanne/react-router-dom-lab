ec0f49328d53c438e48ba0aa2bb60af25cc4ef55
User Name <user@email.com>
2025-03-14 17:08:46 -0400

    fixed spelling issue

--- src/components/MailboxList/MailboxList.jsx
+++ src/components/MailboxList/MailboxList.jsx
@@ -1,13 +1,13 @@
 import {Link} from 'react-router';
-const MailboxList = ({mailBoxes}) => {
-    if (!mailBoxes || mailBoxes.length === 0) {
+const MailboxList = ({mailboxes}) => {
+    if (!mailboxes || mailboxes.length === 0) {
         return <h2>No mailboxes available</h2>
     }
     return (
         <>
             <h2>Mailboxes</h2>
             <ul>
-                {mailBoxes.map((currentMailbox) => (
+                {mailboxes.map((currentMailbox) => (
                     <li key={currentMailbox._id} className='.mail-box'>
                         <Link to={`/mailboxes/${currentMailbox._id}`}>
                         {currentMailbox.boxOwner}
